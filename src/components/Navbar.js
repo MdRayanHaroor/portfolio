de023ad87b9e3354670b33dd91d0a32f659dbdcc
Mohamed Rayan <mohammedrayan977@gmail.com>
2024-11-23 15:15:48 +0530

    Existing changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -95,7 +95,7 @@ function NavBar() {
 
             {/* <Nav.Item>
               <Nav.Link
-                href="https://soumyajitblogs.vercel.app/"
+                href="https://portfolio-mdrayan.vercel.app/"
                 target="_blank"
                 rel="noreferrer"
               >
