8caf1b86f4ecbb5b2830d638917f753686304c28
Mohamed Rayan <mohammedrayan977@gmail.com>
2025-01-27 20:00:25 +0530

    Resume updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,6 +13,7 @@ import {
   AiOutlineHome,
   AiOutlineFundProjectionScreen,
   AiOutlineUser,
+  AiFillGithub,
 } from "react-icons/ai";
 
 import { CgFileDocument } from "react-icons/cg";
@@ -33,90 +34,90 @@ function NavBar() {
 
   return (
     <Navbar
-      expanded={expand}
-      fixed="top"
-      expand="md"
-      className={navColour ? "sticky" : "navbar"}
+  expanded={expand}
+  fixed="top"
+  expand="md"
+  className={navColour ? "sticky" : "navbar"}
+>
+  <Container>
+    <Navbar.Brand href="/" className="d-flex">
+      <img src={logo} className="img-fluid logo" alt="brand" />
+    </Navbar.Brand>
+    <Navbar.Toggle
+      aria-controls="responsive-navbar-nav"
+      onClick={() => {
+        updateExpanded(expand ? false : "expanded");
+      }}
     >
-      <Container>
-        <Navbar.Brand href="/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" />
-        </Navbar.Brand>
-        <Navbar.Toggle
-          aria-controls="responsive-navbar-nav"
-          onClick={() => {
-            updateExpanded(expand ? false : "expanded");
-          }}
-        >
-          <span></span>
-          <span></span>
-          <span></span>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ms-auto" defaultActiveKey="#home">
-            <Nav.Item>
-              <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
-                <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
-              </Nav.Link>
-            </Nav.Item>
+      <span></span>
+      <span></span>
+      <span></span>
+    </Navbar.Toggle>
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="ms-auto" defaultActiveKey="#home">
+        <Nav.Item>
+          <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
+            <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
+          </Nav.Link>
+        </Nav.Item>
 
-            <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/about"
-                onClick={() => updateExpanded(false)}
-              >
-                <AiOutlineUser style={{ marginBottom: "2px" }} /> About
-              </Nav.Link>
-            </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            as={Link}
+            to="/about"
+            onClick={() => updateExpanded(false)}
+          >
+            <AiOutlineUser style={{ marginBottom: "2px" }} /> About
+          </Nav.Link>
+        </Nav.Item>
 
-            <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/project"
-                onClick={() => updateExpanded(false)}
-              >
-                <AiOutlineFundProjectionScreen
-                  style={{ marginBottom: "2px" }}
-                />{" "}
-                Projects
-              </Nav.Link>
-            </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            as={Link}
+            to="/project"
+            onClick={() => updateExpanded(false)}
+          >
+            <AiOutlineFundProjectionScreen style={{ marginBottom: "2px" }} />{" "}
+            Projects
+          </Nav.Link>
+        </Nav.Item>
 
-            <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/resume"
-                onClick={() => updateExpanded(false)}
-              >
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-              </Nav.Link>
-            </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            as={Link}
+            to="/resume"
+            onClick={() => updateExpanded(false)}
+          >
+            <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
+          </Nav.Link>
+        </Nav.Item>
 
-            {/* <Nav.Item>
-              <Nav.Link
-                href="https://portfolio-mdrayan.vercel.app/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <ImBlog style={{ marginBottom: "2px" }} /> Blogs
-              </Nav.Link>
-            </Nav.Item> */}
+        <Nav.Item>
+          <Nav.Link
+            href="https://github.com/MdRayanHaroor"
+            target="_blank"
+            rel="noreferrer"
+            className="icon-color"
+            style={{ top:"2.5px" ,fontSize: "1.5rem", display: "flex", alignItems: "center", justifyContent: "center" }}
+          >
+            <AiFillGithub />
+          </Nav.Link>
+        </Nav.Item>
 
-            <Nav.Item className="fork-btn">
-              <Button
-                href="https://github.com/MdRayanHaroor/portfolio"
-                target="_blank"
-                className="fork-btn-inner"
-              >
-                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
-                <AiFillStar style={{ fontSize: "1.1em" }} />
-              </Button>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        <Nav.Item className="fork-btn">
+          <Button
+            href="https://github.com/MdRayanHaroor/portfolio"
+            target="_blank"
+            className="fork-btn-inner"
+          >
+            <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
+            <AiFillStar style={{ fontSize: "1.1em" }} />
+          </Button>
+        </Nav.Item>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
   );
 }
 
